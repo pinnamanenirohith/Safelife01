90e0bd8a1ed3dc5e33b1939cfff002b9ba5b8539
pinnamanenirohith <pinnamanenirohith@gmail.com>
2025-01-08 11:54:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/safelife01/',
-});
+  base: '/Safelife01/', // Replace 'safelife' with your repository name
+})
